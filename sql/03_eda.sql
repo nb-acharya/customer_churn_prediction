590f086a489d7e88d7593c216f874087ab1ca61a
Niroj Bajracharya <nirojb.er.acharya@outlook.com>
2026-08-24 22:54:30 +0545

    sql eda completed

--- sql/03_eda.sql
+++ sql/03_eda.sql
@@ -86,6 +86,259 @@ ORDER BY
     ChurnRate DESC;
 
 
+-- EDA: customer demographics
+
+--senior citizen churn
+select seniorcitizen, count(*) as totalcustomers,
+sum(case when churn = 1 then 1 else 0 end) as churnedcustomers,
+cast(sum(case when churn = 1 then 1 else 0 end) * 100.0 / count(*) as decimal(5,2)) as churnrate
+from telco_churn_clean
+group by seniorcitizen
+order by churnrate desc
+
+
+--partner and churn rate
+SELECT
+    Partner,
+    COUNT(*) AS TotalCustomers,
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY Partner
+ORDER BY ChurnRate DESC;
+
+--dependents and churn rate
+SELECT
+    Dependents,
+    COUNT(*) AS TotalCustomers,
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY Dependents
+ORDER BY ChurnRate DESC;
+
+
+--payment method and churn rate
+SELECT
+    PaymentMethod,
+    COUNT(*) AS TotalCustomers,
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY PaymentMethod
+ORDER BY ChurnRate DESC;
+
+--paperless billing and churn rate
+SELECT
+    PaperlessBilling,
+    COUNT(*) AS TotalCustomers,
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY PaperlessBilling
+ORDER BY ChurnRate DESC;
+
+
+SELECT
+    OnlineSecurity,
+    COUNT(*) AS TotalCustomers,
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY OnlineSecurity
+ORDER BY ChurnRate DESC;
 
 
 
+
+SELECT
+    OnlineBackup,
+    COUNT(*) AS TotalCustomers,
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY OnlineBackup
+ORDER BY ChurnRate DESC;
+
+
+
+--- device protection
+select deviceprotection,count(*) as totalcustomers,
+sum(case when churn = 1 then 1 else 0 end) as ChurnedCustomers,
+cast(sum(case when churn =1 then 1 else 0 end) * 100.0 / count(*)as decimal(5,2)) as churnrate 
+from telco_churn_clean
+group by deviceprotection
+order by churnrate desc
+
+--- tech support
+select techsupport, count(*) as totalcustomers, 
+sum(case when churn = 1 then 1 else 0 end) as churnedcustomers,
+cast(sum(case when churn =1 then 1 else 0 end) * 100.0 /count(*) as decimal(5,2)) as churnrate
+from telco_churn_clean
+group by techsupport
+order by churnrate desc
+
+--- streamingtv
+select streamingtv, count(*) as totalcustomers, 
+sum(case when churn = 1 then 1 else 0 end) as churnedcustomers,
+cast(sum(case when churn =1 then 1 else 0 end) * 100.0 /count(*) as decimal(5,2)) as churnrate
+from telco_churn_clean
+group by streamingtv
+order by churnrate desc
+
+--- streamingmovies
+select streamingmovies, count(*) as totalcustomers, 
+sum(case when churn = 1 then 1 else 0 end) as churnedcustomers,
+cast(sum(case when churn =1 then 1 else 0 end) * 100.0 /count(*) as decimal(5,2)) as churnrate
+from telco_churn_clean
+group by streamingmovies
+order by churnrate desc
+
+--- phoneservice
+select phoneservice, count(*) as totalcustomers, 
+sum(case when churn = 1 then 1 else 0 end) as churnedcustomers,
+cast(sum(case when churn =1 then 1 else 0 end) * 100.0 /count(*) as decimal(5,2)) as churnrate
+from telco_churn_clean
+group by phoneservice
+order by churnrate desc
+
+--- multiplelines
+select multiplelines, count(*) as totalcustomers, 
+sum(case when churn = 1 then 1 else 0 end) as churnedcustomers,
+cast(sum(case when churn =1 then 1 else 0 end) * 100.0 /count(*) as decimal(5,2)) as churnrate
+from telco_churn_clean
+group by multiplelines
+order by churnrate desc
+
+
+
+WITH ContractStats AS
+(
+    SELECT
+        Contract,
+        COUNT(*) AS TotalCustomers,
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers
+    FROM dbo.telco_churn_clean
+    GROUP BY Contract
+)
+SELECT
+    Contract,
+    TotalCustomers,
+    ChurnedCustomers,
+    CAST(
+        ChurnedCustomers * 100.0 / TotalCustomers
+        AS DECIMAL(5,2)
+    ) AS ChurnRate,
+    CAST(
+        TotalCustomers * 100.0 /
+        SUM(TotalCustomers) OVER ()
+        AS DECIMAL(5,2)
+    ) AS CustomerShare
+FROM ContractStats
+ORDER BY ChurnRate DESC;
+
+
+
+--- ranking customer segments by churn rate
+WITH SegmentStats AS
+(
+    SELECT
+        Contract,
+        InternetService,
+        COUNT(*) AS TotalCustomers,
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers
+    FROM dbo.telco_churn_clean
+    GROUP BY
+        Contract,
+        InternetService
+),
+SegmentRates AS
+(
+    SELECT
+        Contract,
+        InternetService,
+        TotalCustomers,
+        ChurnedCustomers,
+        CAST(
+            ChurnedCustomers * 100.0 / TotalCustomers
+            AS DECIMAL(5,2)
+        ) AS ChurnRate
+    FROM SegmentStats
+)
+SELECT
+    Contract,
+    InternetService,
+    TotalCustomers,
+    ChurnedCustomers,
+    ChurnRate,
+    RANK() OVER (
+        ORDER BY ChurnRate DESC
+    ) AS ChurnRateRank
+FROM SegmentRates
+ORDER BY ChurnRateRank;
+
+
+
+
+SELECT
+    CASE
+        WHEN tenure <= 12
+             AND Contract = 'Month-to-month'
+             AND MonthlyCharges >= 70
+            THEN 'High Risk'
+
+        WHEN tenure <= 24
+             AND Contract = 'Month-to-month'
+            THEN 'Medium Risk'
+
+        ELSE 'Lower Risk'
+    END AS RiskSegment,
+
+    COUNT(*) AS TotalCustomers,
+
+    SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) AS ChurnedCustomers,
+
+    CAST(
+        SUM(CASE WHEN Churn = 1 THEN 1 ELSE 0 END) * 100.0
+        / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS ChurnRate
+FROM dbo.telco_churn_clean
+GROUP BY
+    CASE
+        WHEN tenure <= 12
+             AND Contract = 'Month-to-month'
+             AND MonthlyCharges >= 70
+            THEN 'High Risk'
+
+        WHEN tenure <= 24
+             AND Contract = 'Month-to-month'
+            THEN 'Medium Risk'
+
+        ELSE 'Lower Risk'
+    END
+ORDER BY ChurnRate DESC;
